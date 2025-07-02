b932c77f7b444a09da35a969e6910fdd55eab425
kenttsoi <kenttsoi39@gmail.com>
2025-07-02 18:09:20 +0800

    Build React folder structure and use ts instead

---
+++ frontend/src/pages/Test.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Test: React.FC = () => {
+    const [text, setText] = React.useState<string>('');
+
+  return (
+    <div>Test Page</div>
+  )
+}
---
+++ frontend/src/services/api.ts
@@ -0,0 +1,18 @@
+const API_URL = import.meta.env.VITE_API_URL || '/api';
+
+export const annotateText = async (text: any) => {
+    try {
+        const response = await fetch(``, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ text }),
+        });
+        if (!response.ok) {
+            throw new Error('API request failed');
+        }
+        return await response.json();
+    } catch (error) {
+        console.error("[FRONT ERROR] ", error);
+        throw error;
+    }
+}
