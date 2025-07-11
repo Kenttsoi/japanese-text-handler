29721da3fdb677574c7c4abb2e97a59238d789d8
kenttsoi <kenttsoi39@gmail.com>
2025-07-11 17:42:52 +0800

    Fix api with issue of handling long sound in japanese

--- backend/app/controllers/api.py
+++ backend/app/controllers/api.py
@@ -1,6 +1,6 @@
 from app.utils.text_utils import katakana_to_hiragana
 from flask import Blueprint, request, jsonify, Response, current_app
-import json
+import unicodedata
 
 api = Blueprint('api', __name__)
 
@@ -25,13 +25,17 @@ def annotate():
     if not text:
         return jsonify({'error': 'No text provided'})
     parsed = current_app.tagger.parse(text).splitlines()[:-1]
-    result = {'original': [], 'hiragana': [], 'katakana': []}
+    result = {'result':[], 'original': [], 'hiragana': [], 'katakana': []}
     for line in parsed:
         parts = line.split('\t')
         if len(parts) < 2:
             continue
-        hiragana = katakana_to_hiragana(parts[1])
+        char_type = unicodedata.name(parts[0][0], 'None')
+        if 'CJK UNIFIED IDEOGRAPH' in char_type:
+            hiragana = katakana_to_hiragana(parts[1])
+            result['result'].append(hiragana)
+        else:
+            result['result'].append(parts[0])
         result['original'].append(parts[0])
-        result['hiragana'].append(hiragana)
         result['katakana'].append(parts[1])
     return jsonify(result)
--- backend/app/utils/text_utils.py
+++ backend/app/utils/text_utils.py
@@ -1,4 +1,18 @@
 import jaconv
+import unicodedata
+
+vowel_mapping = {
+    'a': 'あ',
+    'i': 'い',
+    'u': 'う',
+    'e': 'い',
+    'o': 'う',
+    'A': 'あ',
+    'I': 'い',
+    'U': 'う',
+    'E': 'い',
+    'O': 'う',
+}
 
 def katakana_to_hiragana(text):
     """
@@ -10,4 +24,18 @@ def katakana_to_hiragana(text):
     Returns:
         str: The converted string in Hiragana.
     """
-    return jaconv.kata2hira(text)
+
+    category = unicodedata.name('ょ', None)
+    result = ""
+    hiragana = jaconv.kata2hira(text)
+    if len(hiragana) > 1:
+        for i, char in enumerate(hiragana):
+            if char == 'ー':
+                vowel = unicodedata.name(hiragana[i - 1])[-1]
+                result += vowel_mapping.get(vowel, vowel)
+            else:
+                result += char
+    else:
+        return jaconv.kata2hira(text)
+    print(result)
+    return result
