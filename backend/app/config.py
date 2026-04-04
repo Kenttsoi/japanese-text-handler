e1be1e9353d00312522340bef1c7a772271c0070
Kent Tsoi <kenttsoi39@gmail.com>
2026-04-05 03:13:28 +0800

    Configure production environment and add gunicorn support

--- backend/app/config.py
+++ backend/app/config.py
@@ -3,6 +3,7 @@ import os
 class Config:
     """Basic Setting"""
     SECRET_KEY = os.getenv('SECRET_KEY', 'default-secret-key')
+    HOST = '0.0.0.0'
     cors_raw = os.getenv('VITE_API_URL', 'http://localhost:5173')
     CORS_ORIGINS = cors_raw.split(',')
     DEBUG = False
@@ -20,10 +21,11 @@ class TestingConfig(Config):
 
 class ProductionConfig(Config):
     DEBUG = False
-    PORT = int(os.getenv('PORT', 80))
+    PORT = int(os.environ.get('PORT', 5000))
 
 config_dict = {
     'development': DevelopmentConfig,
     'testing': TestingConfig,
-    'production': ProductionConfig
+    'production': ProductionConfig,
+    'default': DevelopmentConfig
 }
